28af84ddc5c331fb212b7e0ab673b6e72d663381
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2021-05-05 22:20:59 -0300

    first commit

---
+++ src/stacks/MainStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Preload from '../screens/preload';
+import SignIn from '../screens/signin';
+import SignUp from '../screens/signup';
+
+const Stack = createStackNavigator();
+
+export default () => (
+    <Stack.Navigator>
+        <Stack.Screen name='Preload' component={Preload} />
+        <Stack.Screen name='SignIn' component={SignIn} />
+        <Stack.Screen name='SignUp' component={SignUp} />
+    </Stack.Navigator>
+);
