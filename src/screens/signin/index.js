417f0ecb84ed2621f808e500215404fa36878ce6
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2021-05-18 23:05:29 -0300

    Terminado SignIn e SignUp

--- src/screens/signin/index.js
+++ src/screens/signin/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { UserContext } from '../../contexts/UserContext';
 import { 
     Container,
     InputArea,
@@ -10,6 +12,8 @@ import {
     SignMessageButtonTextBold
  } from './styles';
 
+ import Api from '../../Api';
+
 import SignInput from '../../components/SignInput';
 
 import BarberLogo from '../../assets/barber.svg';
@@ -18,13 +22,36 @@ import LockIcon from '../../assets/lock.svg';
 
 export default () => {
 
+    const { dispatch: userDispatch } = useContext(UserContext);
     const navigation = useNavigation();
 
     const [emailField, setEmailField] = useState('');
     const [passwordField, setPasswordField] = useState('');
 
-    const handleSignClick = () =>{
+    const handleSignClick = async () =>{
+        if(emailField != '' && passwordField != ''){
 
+            let json = await Api.signIn(emailField, passwordField);
+            if(json.token){
+                
+                await AsyncStorage.setItem('token', json.token);
+                userDispatch({
+                    type: 'setAvatar',
+                    payload: {
+                        avatar: json.data.avatar
+                    }
+                });
+                navigation.reset({
+                    routes:[{name: 'MainTab'}]
+                });
+
+            }else{
+                alert('E-mail e/ou Senha Inválidos!!!');
+            }
+
+        }else{
+            alert("Preencha os campos!");
+        }
     }
 
     const handleMassageButtonClick = () =>{
--- src/screens/signup/index.js
+++ src/screens/signup/index.js
@@ -12,6 +12,8 @@ import {
 
 import SignInput from '../../components/SignInput';
 
+import Api from '../../Api';
+
 import BarberLogo from '../../assets/barber.svg';
 import PersonIcon from '../../assets/person.svg';
 import EmailIcon from '../../assets/email.svg';
@@ -19,11 +21,28 @@ import LockIcon from '../../assets/lock.svg';
 
 export default () => {
 
+    
+
     const navigation = useNavigation();
 
     const [nameField, setNameField] = useState('');
     const [emailField, setEmailField] = useState('');
     const [passwordField, setPasswordField] = useState('');
+
+    const handleSignupClick = async () =>{
+        if(nameField != '' && emailField != '' && passwordField != ''){
+            
+            let resp = await Api.signUp(nameField, emailField, passwordField);
+            if(resp.token){
+                alert("Ok! Usuário criado com sucesso!!!");
+            }else{
+                alert("Erro ao criar usuário! "+ resp.error);
+            }
+
+        }else{
+            alert("Preencha todos os campos!");
+        }
+    }
 
     const handleMassageButtonClick = () =>{
         navigation.reset({
@@ -58,7 +77,7 @@ export default () => {
                     password={true}   
                 />
 
-                <CustomButton>
+                <CustomButton onPress={handleSignupClick}>
                     <CustomButtonText>CADASTRAR</CustomButtonText>
                 </CustomButton>
             </InputArea>
