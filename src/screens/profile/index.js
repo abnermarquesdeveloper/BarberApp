827809668ebf312a3c944e71ae8c40da6f7163c6
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2021-06-11 23:29:50 -0300

    Criado o Logout

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Container } from './styles';
-import {Text} from 'react-native';
+import {Text, Button} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import Api from '../../Api';
 
 export default () =>{
+
+    const navigation = useNavigation();
+
+    const handleLogoutClick = async () => {
+        await Api.logOut();
+        navigation.reset({
+            routes: [{name: 'SignIn'}]
+        });
+    }
+
     return(
         <Container>
             <Text>PROFILE</Text>
+            <Button title="Sair" onPress={handleLogoutClick}/>
         </Container>
     );
 }
